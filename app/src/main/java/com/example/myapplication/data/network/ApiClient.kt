a5c8b9e4f6fca79498c3357b6e3b19281eb975be
rico020202 <ricoroca2@gmail.com>
2023-11-24 19:56:58 -0600

    feat: add documentation to data

--- app/src/main/java/com/example/myapplication/data/network/ApiClient.kt
+++ app/src/main/java/com/example/myapplication/data/network/ApiClient.kt
@@ -3,8 +3,20 @@ package com.example.myapplication.data.network
 import com.example.myapplication.data.network.model.Country
 import com.example.myapplication.utils.Constants
 
+/**
+ * Class ApiClient used to handle API connections
+ *
+ */
 class ApiClient {
     private lateinit var api: ApiService
+
+    /**
+     * fun getList() Gets list of data from API
+     *
+     * @param date Date that wants to be consulted
+     *
+     * @return List<Country> List of countries
+     */
     suspend fun getList(date: String): List<Country>?{
         api = NetworkModuleDI()
         return try{
