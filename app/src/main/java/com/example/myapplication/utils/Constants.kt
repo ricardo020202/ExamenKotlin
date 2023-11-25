cc3d7adfb50e5ea92cd369a75c7cd13c233be459
rico020202 <ricoroca2@gmail.com>
2023-11-24 20:18:45 -0600

    feat: add documentation to utils

--- app/src/main/java/com/example/myapplication/utils/Constants.kt
+++ app/src/main/java/com/example/myapplication/utils/Constants.kt
@@ -1,6 +1,12 @@
 package com.example.myapplication.utils
 
+/**
+ * Object to keep constants
+ */
 object Constants {
+    // API url
     const val BASE_URL = "https://api.api-ninjas.com/v1/"
+
+    // Token for API auth
     const val TOKEN = "yhX+iUmoqT9NzuozV3MdeA==vslJL55upWAnP5tA"
 }
